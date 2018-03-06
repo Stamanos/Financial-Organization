4ae204a2bc8220b9ff27d086bfeda0f641826f9a
Emmanouil Stamatakis <TheMagicSport@gmail.com>
2018-03-06 12:38:41 +0200

    First commit

---
+++ Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace screen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Algorithms.Date.Day(3, 3, 2018));
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
