a21bec05c93231fb27f1bcd347f098f4b85dae48
DOMAIN\eas <e.stamatakis@dataverse.gr>
2018-08-21 11:30:12 +0300

    Create functionality to add values to the database

--- DataBase/SQLite/SQLite/Program.cs
+++ DataBase/SQLite/SQLite/Program.cs
@@ -11,14 +11,27 @@ namespace SQLite
     {
         static void Main(string[] args)
         {
+            //In case I want to built a database from the beginning
+            string createTable = @"CREATE TABLE IF NOT EXIST
+                                 [NameOfTable] (
+                                 [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                 [Name] NVARCHAR(2048) NULL,
+                                 [Gender] NVARCHAR(2048) NULL)";
+
+            //In case I want to add some values to my Table
+            string insertToTable = "INSERT INTO costs(amount, type, date, description, userStatus, moodLevel, weather, location) values('1.2', 'food-drink', '9/1/2018 12:00:00 AM', 'PIPA', 'inRelationship', '5', 'Peristeri', 'Peristeri')";
 
             using (SQLiteConnection connection = new SQLiteConnection("data source = C:/Users/eas/Desktop/Financial-Organization/DataBase/costs.sqlite"))
             {
                 using (SQLiteCommand cmd = new SQLiteCommand(connection))
                 {
                     connection.Open();
+
+                    //If I want to add values
+                    cmd.CommandText = insertToTable;
+                    cmd.ExecuteNonQuery();
+
                     cmd.CommandText = "SELECT* from costs ";
-                    //cmd.ExecuteNonQuery();
                     using(SQLiteDataReader reader = cmd.ExecuteReader())
                     {
                         while (reader.Read())
