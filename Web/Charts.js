9e7de1eba04f49d46a3b0f1d1b7a38256b3727b0
DOMAIN\eas <e.stamatakis@dataverse.gr>
2018-08-31 16:02:57 +0300

    working on live server after run the node.js

--- Web/Charts.js
+++ Web/Charts.js
@@ -1,20 +1,20 @@
 
-$.getJSON( "./columns.json", function( columns ) {
-    columns.forEach(col => {
-        var columnName = col;
-        var path = "./" + columnName + ".json";
-        $.getJSON( path, function( obj ) {
-        var id = columnName + "Selection";
-        obj.forEach(element => {
-            var x = document.getElementById(id);
-            if(x != null){
-            var c = document.createElement("option");
-            c.text = element;
-            c.value = element;
-            x.options.add(c, 1);
-            console.log(element);
-            }
-        });
-        });
-    });
-});
+// $.getJSON( "./columns.json", function( columns ) {
+//     columns.forEach(col => {
+//         var columnName = col;
+//         var path = "./" + columnName + ".json";
+//         $.getJSON( path, function( obj ) {
+//             var id = columnName + "Selection";
+//             obj.forEach(element => {
+//                 var x = document.getElementById(id);
+//                 if(x != null){
+//                     var c = document.createElement("option");
+//                     c.text = element;
+//                     c.value = element;
+//                     x.options.add(c, 1);
+//                     console.log(element);
+//                 }
+//             });
+//         });
+//     });
+// });
