f7af41bf525c21249ef331db9f11f95496cfb45d
DOMAIN\eas <e.stamatakis@dataverse.gr>
2018-10-09 14:10:09 +0300

    V5 IS FULLY AUTOMATED

---
+++ Web/V5.Auto/AddCost/js/style.js
@@ -0,0 +1,89 @@
+var modal = document.getElementById('cost');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// open the map function
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    
+
+    //to make the center of the map the clients location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+        });
+    }
+}
+
+function searchingLocation() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("locationSelection");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myLocations");
+    li = ul.getElementsByTagName("li");
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+//_______________________Insert to Database from Users Input______________________________
+
+function makeNewOutlay(){
+    var amount = document.getElementById("amount").value;
+    var type = document.getElementById("typeSelection").value;
+    var date = document.getElementById("date").value;
+    var description = document.getElementById("description").value;
+    var userStatus = document.getElementById("userStatusSelection").value;
+    var moodLevel = document.getElementById("moodLevelSelection").value;
+    var weather = "NULL";
+    var location = document.getElementById("locationSelection").value;
+}
+
+//_________________________SELECT LOCATION__________________________
+function getEventTarget(e) {
+    e = e || window.event;
+    return e.target || e.srcElement; 
+}
+
+//That is for making selections by the Json files
+$.getJSON( "./../Json/columns.json", function( columns ) {
+    columns.forEach(col => {
+        var columnName = col;
+        var path = "./../Json/" + columnName + ".json";
+        $.getJSON( path, function( obj ) {
+            var id = columnName + "Selection";
+            obj.forEach(element => {
+                var x = document.getElementById(id);
+                if(id == "locationSelection"){
+                    var c = document.createElement("option");
+                    c.setAttribute("value", element);
+                    x.appendChild(c);
+                }
+                else if(x != null){
+                    var c = document.createElement("option");
+                    c.text = element;
+                    c.value = element;
+                    x.options.add(c, 1);
+                }
+            });
+        });
+    });
+});
---
+++ Web/V5.Auto/StartingPage/js/Charts.js
@@ -0,0 +1,223 @@
+let spendingItems, startDate, endDate, startAmount, endAmount;
+
+function showChart() {
+    var chartamount = new CanvasJS.Chart("chartContainer", {
+      title:{
+        text: "amount of money has been spend"              
+      },
+      data: [//array of dataSeries              
+        { //dataSeries object
+          //ToDo:: put the dates as labels
+         type: chartFilters(),
+         dataPoints : contentFilters()
+         }
+       ],
+     });
+    chartamount.render();
+  }
+// functions that modify the spendingItems by the given filters
+//#region Filtering
+function contentFilters(){
+  //declare values
+  startAmount = $( "#slider-range" ).slider( "values", 0 );
+  endAmount = $( "#slider-range" ).slider( "values", 1 );
+  
+  var typeFilter = document.getElementById("typeSelection").value;
+  var dateFilter = document.getElementById("dateCheckbox").checked;
+  var descriptionFilter = document.getElementById("searchDescriptionInput").value;
+  var userStatusFilter = document.getElementById("userStatusSelection").value;
+  var moodLevelFilter = document.getElementById("moodLevelSelection").value;
+  var locationFilter = document.getElementById("locationSelection").value;
+  
+  const result =  spendingItems.
+  filter(function(s){//Cost Amount filter
+    var cost = parseFloat(s[" amount "]);
+    return (parseFloat(startAmount) <= cost && cost <= parseFloat(endAmount));
+  }).
+  filter(function(s){if(typeFilter !== "null") {return s.type == typeFilter;} else {return true;}}).
+  filter(function(s){
+    if(dateFilter) {
+      //creating the cost date as date time
+      var costDate_array = String(s.date).split('/');
+      var costDate = new Date("20" + costDate_array[2], costDate_array[0] - 1, costDate_array[1]);
+      //creating the filter date as date time (Start Date)
+      var filterStartDate_array = startDate.format('YYYY-MM-DD').split('-');
+      var filterStartDate = new Date(filterStartDate_array[0], filterStartDate_array[1] - 1, filterStartDate_array[2]);
+      //creating the filter date as date time (End Date)
+      var filterEndDate_array = endDate.format('YYYY-MM-DD').split('-');
+      var filterEndDate = new Date(filterEndDate_array[0], filterEndDate_array[1] - 1, filterEndDate_array[2]);
+      return ((+filterStartDate.getTime() <= +costDate.getTime()) && (+costDate.getTime() <= +filterEndDate.getTime()));
+    }
+    else {
+      return true;
+    }}).
+  filter(function(s){return String(s.description).includes(descriptionFilter);}).
+  filter(function(s){if(userStatusFilter !== "null") {return s.userStatus == userStatusFilter;} else {return true;}}).
+  filter(function(s){if(moodLevelFilter !== "null") {return s.moodLevel == moodLevelFilter;} else {return true;}}).
+  filter(function(s){if(locationFilter !== "null") {return s.location == locationFilter;} else {return true;}});
+  
+  //This is for creating inner Html for costs
+  var amountlist = result.map(i => {
+    return parseFloat(i[" amount "]);
+  });
+  TotalCost(amountlist);
+
+  return result.map(i => {
+    return {label: i.description, y: parseFloat(i[" amount "])};
+  });
+}
+//#endregion
+
+function chartFilters(){
+  return document.getElementById("typeOfChartSelection").value;
+}
+
+function Refresh(){
+  showChart();
+}
+
+//#region Column Filtering
+function columnFilters(){
+  var column = document.getElementById("columnsSelection").value;
+  const chartData = {};
+  $.getJSON("./Json/"+ column +".json", function(elements){
+    elements.forEach(element => {
+        var temp = spendingItems.filter(function(s){
+            return s[`${column}`] == element; //searching for costs that has this characteristic
+        });
+        var amount = temp.map(a => {
+            return parseFloat(a[" amount "]); //collect the list of amounts
+        });
+        chartData[element] = amount.reduce((a,b) => a + b, 0).toFixed(2); //push the sum of it to the name of...Element
+        var data = {};
+        for(var key in chartData){
+          data.label = key;
+          data.cost = chartData[key];
+        }
+        console.log(data);
+      });
+  });
+}
+//#endregion
+
+//#region total-cost-to-inner-html
+function TotalCost(amountList){
+  var sum = amountList.reduce((a,b) => a + b, 0).toFixed(2);
+
+  $('#totalCost').empty(); //clean the previous if exists!
+  var myDiv = document.getElementById("totalCost");
+  var h2 = document.createElement("h2");
+  h2.textContent = "Total cost: " + sum.toString();
+  myDiv.appendChild(h2);
+}
+//#endregion
+
+//#region ExcelToJson
+$(function() {
+  var oFileIn;
+  oFileIn = document.getElementById('my_file_input');
+  if(oFileIn.addEventListener) {
+      oFileIn.addEventListener('change', filePicked, false);
+  }
+});
+
+function filePicked(oEvent) {
+  // Get The File From The Input
+  var oFile = oEvent.target.files[0];
+  // Create A File Reader HTML5
+  var reader = new FileReader();
+  // Ready The Event For When A File Gets Selected
+  reader.onload = function(e) {
+    var data = e.target.result;
+    var cfb = XLSX.read(data, {type: 'binary'});
+    console.log(cfb)
+    cfb.SheetNames.forEach(function(sheetName) {
+      spendingItems = XLS.utils.sheet_to_json(cfb.Sheets[sheetName]);   
+      console.log(spendingItems);
+      createHTML();
+    });
+  };
+  // Tell JS To Start Reading The File.. You could delay this if desired
+  reader.readAsBinaryString(oFile);
+}
+//#endregion
+
+//#region Calendar
+$(function() {
+  var start = moment().subtract(29, 'days');
+  var end = moment();
+
+  function cb(start, end) {
+      $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+      startDate = start;
+      endDate = end;
+  }
+  $('#reportrange').daterangepicker({
+      startDate: start,
+      endDate: end,
+      ranges: {
+         'Today': [moment(), moment()],
+         'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+         'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+         'This Month': [moment().startOf('month'), moment().endOf('month')],
+         'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      }
+  }, cb);
+  cb(start, end);
+});
+//#endregion
+
+//#region Slider
+$( function() {
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [ 0, 500 ],
+    slide: function( event, ui ) {
+      $( "#amountRange" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
+    }
+  });
+  $( "#amountRange" ).val( "€" + $( "#slider-range" ).slider( "values", 0 ) +
+    " - €" + $( "#slider-range" ).slider( "values", 1 ) );
+});
+//#endregion
+
+//#region innnerHtml
+function createHTML(){
+    var uniqueValues = [];
+    for(var key in spendingItems[0]){
+      for(var i = 0; i<spendingItems.length; i++){
+        if(uniqueValues.indexOf(spendingItems[i][`${key}`]) === -1){
+          uniqueValues.push(spendingItems[i][`${key}`]);
+        }
+      }
+      console.log(uniqueValues);
+      if(key !== 'date' && key !== ' amount ' && key !== 'weather' && key !== 'description'){
+        makeSelection(key, uniqueValues);
+      }
+      uniqueValues = [];
+    }
+      
+}
+
+function makeSelection(input, optionList){
+  var myDiv = document.getElementById("autoGenerated");
+  var newLabel = document.createElement("label");
+  var newSelection = document.createElement("select");
+  
+  newLabel.textContent = input;
+  newSelection.id = input + "Selection";
+  
+  myDiv.appendChild(newLabel);
+  myDiv.appendChild(newSelection);
+
+  optionList.forEach(opt => {
+      var option = document.createElement("option");
+      option.text = opt;
+      option.value = opt;
+      newSelection.appendChild(option);
+  });
+}
+//#endregion
