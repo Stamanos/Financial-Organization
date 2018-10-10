2f27a1e2d083b1640c44535064304192a72c7313
Emmanouil Stamatakis <TheMagicSport@gmail.com>
2018-10-10 22:28:29 +0300

    Chart remove comments

--- Web/V5.Auto/StartingPage/js/Charts.js
+++ Web/V5.Auto/StartingPage/js/Charts.js
@@ -5,9 +5,8 @@ function showChart() {
       title:{
         text: "amount of money has been spend"              
       },
-      data: [//array of dataSeries              
-        { //dataSeries object
-          //ToDo:: put the dates as labels
+      data: [             
+        {
          type: chartFilters(),
          dataPoints : contentFilters()
          }
