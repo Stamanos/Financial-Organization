cd06c7ad25fb47e77147c750326149ee85e3dff4
Manos Stamatakis <TheMagicSport@gmail.com>
2018-11-15 14:15:19 +0200

    bug fix

--- Web/morgan-app/src/App.js
+++ Web/morgan-app/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import daterangepicker, { Calendar } from './Calendar';
-import amountSlider, { Slider } from './Slider';
-import createHTML, { SelectionFilters } from './InnerHtml/SelectionFilters';
-import totalCost, { TotalCost } from './InnerHtml/TotalCost';
-import columnFilters, { ColumnFilters } from './Filters/ColumnFilters';
-import contentFilters, { ContentFilters } from './Filters/ContentFilters';
-import showChart, { DisplayCharts } from './DisplayCharts';
-import excelToJson, { ExcelConverter } from './ExcelConverter';
-import filterByDate, { DateFilter } from './Filters/DateFilter';
+import Calendar, { daterangepicker } from './Calendar';
+import Slider, { amountSlider } from './Slider';
+import SelectionFilters, { createHTML } from './InnerHtml/SelectionFilters';
+import TotalCost, { totalCost } from './InnerHtml/TotalCost';
+import ColumnFilters, { columnFilters } from './Filters/ColumnFilters';
+import ContentFilters, { contentFilters } from './Filters/ContentFilters';
+import DisplayCharts, { showChart } from './DisplayCharts';
+import ExcelConverter, { excelToJson } from './ExcelConverter';
+import DateFilter, { filterByDate } from './Filters/DateFilter';
 
 //let spendingItems, startDate, endDate, startAmount, endAmount, excelColumns;
 
