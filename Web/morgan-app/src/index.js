61e276e54e1e53ef8b7806dae2b84f572189b2c2
Manos Stamatakis <TheMagicSport@gmail.com>
2018-11-15 15:02:34 +0200

    change the imports

--- Web/morgan-app/src/index.js
+++ Web/morgan-app/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import daterangepicker from './calendar';
+import amountSlider from './slider';
+import createHTML from './InnerHtml/selectionFilters';
+import totalCost from './InnerHtml/totalCost';
+import columnFilters from './Filters/columnFilters';
+import contentFilters from './Filters/contentFilters';
+import showChart from './displayCharts';
+import excelToJson from './excelConverter';
+import filterByDate from './Filters/dateFilter';
 
 const element = <h1>Hello world</h1>;
-
 console.log(element);
+ReactDOM.render(element, document.getElementById('root'));
