275be47459ca05afc4c34fbc26df3ba4188c1214
Manos Stamatakis <TheMagicSport@gmail.com>
2018-11-15 15:27:09 +0200

    Ela ntaks to exw

--- Web/morgan-app/src/index.js
+++ Web/morgan-app/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import daterangepicker from './calendar';
-import amountSlider from './slider';
-import createHTML from './InnerHtml/selectionFilters';
-import totalCost from './InnerHtml/totalCost';
-import columnFilters from './Filters/columnFilters';
-import contentFilters from './Filters/contentFilters';
-import showChart from './displayCharts';
-import excelToJson from './excelConverter';
-import filterByDate from './Filters/dateFilter';
+//import daterangepicker from './calendar';
+//import amountSlider from './slider';
+//import createHTML from './InnerHtml/selectionFilters';
+//import totalCost from './InnerHtml/totalCost';
+//import columnFilters from './Filters/columnFilters';
+//import contentFilters from './Filters/contentFilters';
+//import showChart from './displayCharts';
+//import excelToJson from './excelConverter';
+//import filterByDate from './Filters/dateFilter';
 
 const element = <h1>Hello world</h1>;
 console.log(element);
