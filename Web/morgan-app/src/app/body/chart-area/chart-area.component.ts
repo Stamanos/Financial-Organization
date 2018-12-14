6828120113e9ca1c7db1d994a474cdb11bf5368c
Manos Stamatakis <TheMagicSport@gmail.com>
2018-12-14 15:06:28 +0200

    import charts functionality

--- Web/morgan-app/src/app/body/chart-area/chart-area.component.ts
+++ Web/morgan-app/src/app/body/chart-area/chart-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './../../canvasjs.min';
 
 @Component({
   selector: 'app-chart-area',
@@ -10,6 +11,31 @@ export class ChartAreaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Food" },
+          { y: 120, name: "Insurance" },
+          { y: 300, name: "Traveling" },
+          { y: 800, name: "Housing" },
+          { y: 150, name: "Education" },
+          { y: 150, name: "Shopping"},
+          { y: 250, name: "Others" }
+        ]
+      }]
+    });
+      
+    chart.render();
   }
 
 }
--- Web/morgan-app/src/app/sidebar/charts/charts.component.ts
+++ Web/morgan-app/src/app/sidebar/charts/charts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './../../canvasjs.min';
 
 @Component({
   selector: 'app-charts',
@@ -10,6 +11,81 @@ export class ChartsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let pieChart = new CanvasJS.Chart("pieChartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Food" },
+          { y: 120, name: "Insurance" },
+          { y: 300, name: "Traveling" },
+          { y: 800, name: "Housing" },
+          { y: 150, name: "Education" },
+          { y: 150, name: "Shopping"},
+          { y: 250, name: "Others" }
+        ]
+      }]
+    });
+
+    let areaChart = new CanvasJS.Chart("areaChartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "area",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Food" },
+          { y: 120, name: "Insurance" },
+          { y: 300, name: "Traveling" },
+          { y: 800, name: "Housing" },
+          { y: 150, name: "Education" },
+          { y: 150, name: "Shopping"},
+          { y: 250, name: "Others" }
+        ]
+      }]
+    });
+
+    let barChart = new CanvasJS.Chart("barChartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "bar",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Food" },
+          { y: 120, name: "Insurance" },
+          { y: 300, name: "Traveling" },
+          { y: 800, name: "Housing" },
+          { y: 150, name: "Education" },
+          { y: 150, name: "Shopping"},
+          { y: 250, name: "Others" }
+        ]
+      }]
+    });
+      
+    pieChart.render();
+    areaChart.render();
+    barChart.render();
   }
 
 }
